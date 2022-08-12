dfb49f6e453cbca76a7e7965c19d34fc00f0be83
syrtsovaiv <102823286+syrtsovaiv@users.noreply.github.com>
2022-08-12 13:31:13 +0300

    first try

--- new_project/src/main/java/org/example/myfirstClass.java
+++ new_project/src/main/java/org/example/myfirstClass.java
@@ -1,6 +1,6 @@
 package org.example;
 
-public class MyfirstClass {
+public class myfirstClass {
     public static void main(String[] args) {
         System.out.println("namaste, mutafakaz!");
     }
